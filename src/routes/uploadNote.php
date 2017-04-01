c6766947bb284f85b155dd03124aa1b4b4a35226
Stefan Skliarov <serg.osipchuk@rapidapi.com>
2017-04-01 13:27:49 +0300

    note upload checking added

--- src/routes/uploadNote.php
+++ src/routes/uploadNote.php
@@ -22,12 +22,15 @@ $app->post('/api/Evernote/uploadNote', function ($request, $response, $args) {
     try {
         $note = new \Evernote\Model\Note();
         $note->title = $post_data['args']['noteTitle'];
+        if (isset($post_data['args']['noteContent']) && strlen($post_data['args']['noteContent']) > 0) {
+            $note->content = new \Evernote\Model\PlainTextNoteContent($post_data['args']['noteContent']);
+        }
         $note->content = new \Evernote\Model\PlainTextNoteContent($post_data['args']['noteContent']);
 
-        if (isset($post_data['args']['noteTags'])) {
+        if (isset($post_data['args']['noteTags']) && strlen($post_data['args']['noteTags']) > 0) {
             $note->tagNames = $post_data['args']['noteTags'];
         }
-        if (isset($post_data['args']['notebook'])) {
+        if (isset($post_data['args']['notebookGuid']) && strlen($post_data['args']['notebookGuid']) > 0) {
             $notebook = $client->getNotebook($post_data['args']['notebookGuid']);
 
         }
